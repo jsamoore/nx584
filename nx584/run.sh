7c42e972c17fb367d5d8befeb7c6f053b9b6606e
jsamoore <145287058+jsamoore@users.noreply.github.com>
2023-09-18 19:09:52 -0400

    Update run.sh

--- nx584/run.sh
+++ nx584/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/with-contenv bashio
-python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 192.168.0.82:10000 --config /home/pi/tcp-serial/config.ini
+python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 192.168.0.82:10000 --config /home/pi/config.ini
